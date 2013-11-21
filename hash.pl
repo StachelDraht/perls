507b8a0b6bfde4affc7b5c1d18b0c630325ba09f
Maksim Ka <maksim.karpenko@gmail.com>
2013-11-21 22:24:04 +0200

    cont Signed-off-by: Maksim Ka <maksim.karpenko@gmail.com>

--- hash.pl
+++ hash.pl
@@ -1,6 +1,10 @@
 use strict;
 use warnings;
+use Data::Dumper;
 
-my %hash = ('beer'=> 'dark');
-my $name = "Maksim";
-
+my %hash = ('beer'=> 'dark', 'language'=>'strange');
+#my $name = "Maksim";
+#print Dumper(%hash);
+while(my ($key, $value) = each %hash) {
+	print "$key => $value \n";
+}
