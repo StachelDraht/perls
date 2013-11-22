525ec8c0d4a439c524cdd061ff9206f0c1a13275
Maksim Ka <maksim.karpenko@gmail.com>
2013-11-22 19:16:52 +0200

    small socket client / part of chat

--- sockets.pl
+++ sockets.pl
@@ -1,20 +1,41 @@
+use 5.010;
+
 use strict;
 use warnings;
 use Socket;
 
-my $host = "eeelhd";
-my $port = "80";
-
-socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
-
+my $host = "164.48.7.92";
+my $port = "21";
+say "#######";
+say "##";
+say "##";
+say "##";
+say "##";
+say "#######";
+say "Input your name";
+my $name = <STDIN>;
+chomp $name;
+my $remote = socket(SOCK, PF_INET, SOCK_STREAM, getprotobyname('tcp'));
 my $iaddr = inet_aton($host);
 my $paddr = sockaddr_in($port, $iaddr);
 
 connect(SOCK, $paddr);
-send (SOCK, "GET / HTTP/1.0\n\n", 0);
+print "Welcome to server, $name! \n";
+#send (SOCK, "GET / HTTP/1.0\n\n", 0);
 
-my @data=<SOCK>;
+send(SOCK, "$name", 0);
+
+my $line;
+#my @data=<SOCK>;
+while ($line = <SOCK>) {
+	print "Server: $line \n";
+	print "message: ";
+	my $send = <STDIN>;
+	send(SOCK, "$name: $send", 0);
+}
+
+
+
 
 close(SOCK);
-
-print @data;
+#say @data;
